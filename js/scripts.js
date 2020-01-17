ddc3d9e2c52072b049efbd97da8de5e010198949
Abraham Ybarra <abeybarra@gmail.com>
2020-01-17 08:43:27 -0800

    add js for #gemInput variable and event.preventDefault()

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
- 
+$(document).ready(function(){
+  $("#gemInput").submit(funtion(event(){
+    var gem = $("input:radio[name=Gems]:checked").val();
+
+  }
+
+
+
+
+
+
+    event.preventDefault();
+  });
+});
