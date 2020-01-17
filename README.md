f16e1c6ebd1a088c96dc1f9aa7c59497a85b2d03
Abraham Ybarra <abeybarra@gmail.com>
2020-01-17 15:17:08 -0800

    messed around with if else operators, updated README.md, added more css styles and a short introduction

---
+++ README.md
@@ -0,0 +1,42 @@
+# WEEK 2 INDEPENDENT PROJECT
+
+## AKA "Code Survey"
+
+### My Second independant project at Epicodus
+Created January 17th, 2020
+### By Abraham Ybarra III
+
+## Description
+
+---
+
+This was my second independant project at Epicodus coding school in Portland, OR. It's a simple multiple choice questionaire used to determine the best coding language for you to learn based on your answers.
+
+## Setup
+
+* Clone the repository for this project to your desktop.
+* Open files.
+* You can also view the page live in GitHub Pages(link below).
+
+## GitHub Pages
+
+View the page live at https://abeybarra.github.io/
+
+## Known Bugs
+
+Non that I know of, but with a little practice I'm hoping to get it looking nicer over time.
+
+---
+
+## Technologies Used
+
+* Git
+* Virtual Studio Code
+* HTML
+* Markdown
+* CSS
+* Bootstraps
+* Javascript
+* JQuery
+---
+Copyright (c) 2020 **Abraham Ybarra III**
--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,32 @@ $(document).ready(function(){
     var item = $("input:radio[name=items]:checked").val();
     var day = $("input:radio[name=days]:checked").val();
     var monster =$("input:radio[name=monsters]:checked").val();
-    var swallow =$("input:radio[name=swallows]:checked").val
+    var swallow =$("input:radio[name=swallows]:checked").val()
 
-    if(item === "ruby" && day === "mining" && monster === "medusa" || monster === "dio"){
+    if(item === "ruby" && day === "mining" && monster === "rock"){
       $("#ruby").show();
       $("#python").hide();
-      $("#go").hide();
-    }else if(item === "snake" && day === "zoo" && monster === "rock" || monster === "dio"){
+      $("#go").hide();    
+
+    }else if(item === "snake" && day === "zoo" && monster === "medusa"){
       $("#ruby").hide();
       $("#python").show();
       $("#go").hide();
-    }else if(item === "game" && day === "greenLight" && monster === "rock" || monster === "medusa"){
+    
+    }else if(item === "game" && day === "greenLight" && monster === "dio"){
       $("#ruby").hide();
       $("#python").hide();
       $("#go").show();
-    }else if(item === "ruby" && day === "zoo" || day === "greenLight" && monster === "medusa" || monster === "dio"){
-      $("#ruby").show();
+
+    }else {
+      $("#unknown").show();
+      $("#ruby").hide();
       $("#python").hide();
       $("#go").hide();
-    }else if(item === "ruby" && day === "mining" && monster === "rock"){
-      $("#ruby").show();
-      $("#python").hide();
-      $("#go").hide();
-    }else if(item === "snake" || item === "game" && day === "mining" && monster === "medusa" || monster === "dio"){
-      $("#ruby").show();
-      $("#python").hide();
-      $("#go").hide();
+
     }
+
+    
 
 
 
