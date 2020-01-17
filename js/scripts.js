898afad9ddb01eaf89d5a5ddf765ffef2b44fe9f
Abraham Ybarra <abeybarra@gmail.com>
2020-01-17 10:57:07 -0800

    add panel and images for results, style with css

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
-  $("#gemInput").submit(function(event){
+  $("#formInput").submit(function(event){
     var item = $("input:radio[name=items]:checked").val();
     var day = $("input:radio[name=days]:checked").val();
 
 
     if(item === "red"){
       $("#ruby").show();
-      $("python").hide();
-      $("go").hide();
+      $("#python").hide();
+      $("#go").hide();
+    }else if(item === "slithering"){
+      $("#ruby").hide();
+      $("#python").show();
+      $("#go").hide();
+    }else if(item === "game"){
+      $("#ruby").hide();
+      $("#python").hide();
+      $("#go").show();
     }
 
 
